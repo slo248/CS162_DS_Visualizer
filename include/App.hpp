68ae05db8d1d6532e69d2dcf7c37ac6a80b333fd
Nguyễn Hoàng Phúc <nhplhp2004@gmail.com>
2023-03-20 22:03:53 +0700

    change data type of App::TimePerFrame

--- include/App.hpp
+++ include/App.hpp
@@ -11,7 +11,7 @@ class App
     private:
         const int                   windowWidth=1000;
         const int                   windowHeight=1000;
-        const float                 TimePerFrame=1.f/60.f;
+        const sf::Time              TimePerFrame=sf::seconds(1.f/60.f);
 
     private:
         sf::RenderWindow            mWindow;
