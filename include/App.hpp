e605b4a2eef8dc8ecff0b2062b601ea6a0af07c5
Nguyễn Hoàng Phúc <nhplhp2004@gmail.com>
2023-03-20 21:54:20 +0700

    add const App::TimePerFrame

--- include/App.hpp
+++ include/App.hpp
@@ -11,6 +11,7 @@ class App
     private:
         const int   windowWidth=1000;
         const int   windowHeight=1000;
+        const float TimePerFrame=1.f/60.f;
 
     private:
         sf::RenderWindow            mWindow;
