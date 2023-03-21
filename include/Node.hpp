a858382845efb9af75060067095e200fe643023d
Nguyễn Hoàng Phúc <nhplhp2004@gmail.com>
2023-03-21 19:03:55 +0700

    add Node destructor

--- include/Node.hpp
+++ include/Node.hpp
@@ -3,6 +3,7 @@
 class Node{
     public:
         Node(int _val, Node* _pNext, Node* _pPrev);
+        ~Node();
     private:
         int val;
         Node *pNext, *pPrev;
--- src/Node.cpp
+++ src/Node.cpp
@@ -4,3 +4,7 @@ Node::Node(int _val=0, Node *_pNext=nullptr, Node *_pPrev=nullptr):
     val(_val),pNext(_pNext),pPrev(_pPrev)
 {
 }
+
+Node::~Node()
+{
+}
