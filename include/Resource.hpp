168b7b371b20a29d78c3aa487eb2367ab0ca5540
Nguyễn Hoàng Phúc <nhplhp2004@gmail.com>
2023-03-21 15:27:54 +0700

    add Resource.hpp

---
+++ include/Resource.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <map>
+#include <memory>
+#include <SFML/Graphics.hpp>
+
+namespace Fonts{
+    enum ID{
+        Arial,
+        Consolas,
+        Times
+    };
+}
+
+template<class Resource, class Identifier>
+class ResourceHolder{
+    private:
+        std::map<Identifier,std::unique_ptr<Resource>> holder;
+    
+};
