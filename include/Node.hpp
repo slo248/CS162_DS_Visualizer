690dbac738190ae93013ad8ca16388860a32c2c4
Nguyễn Hoàng Phúc <nhplhp2004@gmail.com>
2023-03-21 19:29:29 +0700

    add Node::mCircle and update Node constructor

--- include/Node.hpp
+++ include/Node.hpp
@@ -18,6 +18,8 @@ class Node: public SceneNode{
         int val;
         Node *pNext, *pPrev;
 
+        sf::CircleShape     mCircle;
+
     private:
         virtual void    drawCurrent(sf::RenderTarget& target,sf::RenderStates states) const;
 };
--- src/Node.cpp
+++ src/Node.cpp
@@ -3,6 +3,14 @@
 Node::Node(int _val=0, Node *_pNext=nullptr, Node *_pPrev=nullptr):
     val(_val),pNext(_pNext),pPrev(_pPrev)
 {
+    mCircle.setPosition(100.f,100.f);
+    mCircle.setRadius(10.f);
+    mCircle.setFillColor(sf::Color::White);
+    mCircle.setOutlineThickness(8.f);
+    mCircle.setOutlineColor(sf::Color::Blue);
+
+    sf::FloatRect bounds=mCircle.getLocalBounds();
+    mCircle.setOrigin(bounds.width/2,bounds.height/2);
 }
 
 Node::~Node()
