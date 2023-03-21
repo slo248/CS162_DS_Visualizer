d0aa3700bfab6230c2ec006a8452c81cab5b2edb
Nguyễn Hoàng Phúc <nhplhp2004@gmail.com>
2023-03-21 19:17:18 +0700

    declare Node::drawCurrent()

--- include/Node.hpp
+++ include/Node.hpp
@@ -6,7 +6,11 @@ class Node: public SceneNode{
     public:
         Node(int _val, Node* _pNext, Node* _pPrev);
         ~Node();
+
     private:
         int val;
         Node *pNext, *pPrev;
+
+    private:
+        virtual void    drawCurrent(sf::RenderTarget& target,sf::RenderStates states) const;
 };
