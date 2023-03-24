f8c543ca0082d532d34cb0117b927f514380815e
Nguyễn Hoàng Phúc <nhplhp2004@gmail.com>
2023-03-24 20:33:35 +0700

    add constant data in TextBox

--- include/TextBox.hpp
+++ include/TextBox.hpp
@@ -1,11 +1,16 @@
 #pragma once
 
+#include <string>
 #include <SceneNode.hpp>
+
+const sf::Color         CHAR_COLOR=sf::Color::Red;
+const std::string       DEFAULT_TEXT="hello";
+const sf::Vector2f      DEFAULT_SIZE=sf::Vector2f(100.f,100.f);
 
 class TextBox: public SceneNode
 {
     public:
-    
+
     private:
         sf::Text            mText;
 };
