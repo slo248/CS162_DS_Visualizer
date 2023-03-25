319176efd19381dc4ee551f327bca1ae68db8172
Nguyễn Hoàng Phúc <nhplhp2004@gmail.com>
2023-03-25 16:22:18 +0700

    define Arrow constructor

--- include/Arrow.hpp
+++ include/Arrow.hpp
@@ -5,7 +5,14 @@
 class Arrow: public SceneNode
 {
     private:
+        const float             HEIGHT_EACH=10;
+        const float             FIRST_WIDTH=50;
+        const float             SECOND_WIDTH=20;
+        const sf::Color         DEFAULT_COLOR=sf::Color::Black;
+
+    private:
         sf::ConvexShape mConvex;
+
     public:
         Arrow();
         void draw(sf::RenderTarget& target, sf::RenderStates states) const;
---
+++ src/Arrow.cpp
@@ -0,0 +1,15 @@
+#include <Arrow.hpp>
+
+Arrow::Arrow()
+{
+    mConvex.setPointCount(7);
+    mConvex.setFillColor(DEFAULT_COLOR);
+
+    mConvex.setPoint(0,sf::Vector2f(0,HEIGHT_EACH));
+    mConvex.setPoint(1,sf::Vector2f(FIRST_WIDTH,HEIGHT_EACH));
+    mConvex.setPoint(2,sf::Vector2f(FIRST_WIDTH,HEIGHT_EACH*3.f/2.f));
+    mConvex.setPoint(3,sf::Vector2f(FIRST_WIDTH+SECOND_WIDTH,HEIGHT_EACH*3.f/4.f));
+    mConvex.setPoint(4,sf::Vector2f(FIRST_WIDTH,0));
+    mConvex.setPoint(5,sf::Vector2f(FIRST_WIDTH,HEIGHT_EACH/2.f));
+    mConvex.setPoint(6,sf::Vector2f(0,HEIGHT_EACH/2.f)); 
+}
