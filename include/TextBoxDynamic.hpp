cf72b7322ff322b9d96df26abd65af43c8c4be47
Nguyễn Hoàng Phúc <nhplhp2004@gmail.com>
2023-03-30 22:35:06 +0700

    add constant keys in TextBoxDynamic

--- include/TextBoxDynamic.hpp
+++ include/TextBoxDynamic.hpp
@@ -3,6 +3,10 @@
 #include <sstream>
 #include <SceneNode.hpp>
 
+const int DELETE_KEY   =8;
+const int ENTER_KEY    =13;
+const int ESCAPE_KEY   =27;
+
 class TextBoxDynamic: public SceneNode
 {
     public:
