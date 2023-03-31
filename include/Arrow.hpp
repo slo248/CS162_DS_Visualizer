ff4980693b463e6de141bcfe23066840f37872fb
Nguyễn Hoàng Phúc <nhplhp2004@gmail.com>
2023-03-31 22:53:46 +0700

    change shape of the arrow

--- include/Arrow.hpp
+++ include/Arrow.hpp
@@ -5,13 +5,14 @@
 class Arrow: public SceneNode
 {
     public:
-        const float             HEIGHT_EACH=10.f;
+        const float             HEIGHT_EACH=5.f;
         const float             RATIO_FIRST=3.f/4.f;
         const float             RATIO_SECOND=1-RATIO_FIRST;
         const sf::Color         DEFAULT_COLOR=sf::Color::Blue;
 
     private:
-        sf::ConvexShape mConvex;
+        sf::RectangleShape      mLine;
+        sf::ConvexShape         mTriangle;
 
     public:
         Arrow(sf::Vector2f dist);
--- src/Arrow.cpp
+++ src/Arrow.cpp
@@ -2,30 +2,32 @@
 
 Arrow::Arrow(sf::Vector2f dist)
 {
-    mConvex.setPointCount(7);
-    mConvex.setFillColor(DEFAULT_COLOR);
-    
     const float length=sqrt(dist.x*dist.x+dist.y*dist.y);
-    // std::cout<<length<<' '<<angle<<'\n';
 
     const float FIRST_WIDTH=length*RATIO_FIRST;
     const float SECOND_WIDTH=length*RATIO_SECOND;
-    // std::cout<<FIRST_WIDTH<<' '<<SECOND_WIDTH<<'\n';
 
-    mConvex.setPoint(0,sf::Vector2f(0,HEIGHT_EACH));
-    mConvex.setPoint(1,sf::Vector2f(FIRST_WIDTH,HEIGHT_EACH));
-    mConvex.setPoint(2,sf::Vector2f(FIRST_WIDTH,HEIGHT_EACH*3.f/2.f));
-    mConvex.setPoint(3,sf::Vector2f(FIRST_WIDTH+SECOND_WIDTH,HEIGHT_EACH*3.f/4.f));
-    mConvex.setPoint(4,sf::Vector2f(FIRST_WIDTH,0));
-    mConvex.setPoint(5,sf::Vector2f(FIRST_WIDTH,HEIGHT_EACH/2.f));
-    mConvex.setPoint(6,sf::Vector2f(0,HEIGHT_EACH/2.f)); 
+    mLine.setSize(sf::Vector2f(FIRST_WIDTH,HEIGHT_EACH));
+    mLine.setFillColor(DEFAULT_COLOR);
+    {
+        sf::FloatRect bounds=mLine.getLocalBounds();
+        mLine.setOrigin(0,bounds.top+bounds.height/2);
+    }
 
-    sf::FloatRect bounds=mConvex.getLocalBounds();
-
-    mConvex.setOrigin(bounds.left,bounds.top+bounds.height/2);
+    mTriangle.setPointCount(3);
+    mTriangle.setFillColor(DEFAULT_COLOR);
+    mTriangle.setPoint(0,sf::Vector2f(0,0));
+    mTriangle.setPoint(1,sf::Vector2f(SECOND_WIDTH,HEIGHT_EACH*3.f/2.f));
+    mTriangle.setPoint(2,sf::Vector2f(0,HEIGHT_EACH*3.f));
+    {
+        sf::FloatRect bounds=mTriangle.getLocalBounds();
+        mTriangle.setOrigin(0,bounds.top+bounds.height/2);
+    }
+    mTriangle.move(FIRST_WIDTH,0);
 }
 
 void Arrow::drawCurrent(sf::RenderTarget &target, sf::RenderStates states) const
 {
-    target.draw(mConvex,states);
+    target.draw(mLine,states);
+    target.draw(mTriangle,states);
 }
