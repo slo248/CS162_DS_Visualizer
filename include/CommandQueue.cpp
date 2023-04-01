6192bed8310b7ed92200c57978b42dfeb41310c7
Nguyễn Hoàng Phúc <nhplhp2004@gmail.com>
2023-04-01 21:53:49 +0700

    define CommandQueue::pop()

--- include/CommandQueue.cpp
+++ include/CommandQueue.cpp
@@ -4,3 +4,10 @@ void CommandQueue::push(const Command &command)
 {
     mQueue.push(command);
 }
+
+Command CommandQueue::pop()
+{
+    Command res=mQueue.front();
+    mQueue.pop();
+    return res;
+}
