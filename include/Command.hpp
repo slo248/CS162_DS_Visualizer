f20b275baaa4e07fdbef06c0af4c6026733a6dea
Nguyễn Hoàng Phúc <nhplhp2004@gmail.com>
2023-04-01 23:43:53 +0700

    add template derivedAction()

--- include/Command.hpp
+++ include/Command.hpp
@@ -10,3 +10,16 @@ struct Command
     int category;
     std::function<void(SceneNode&, sf::Time)> action;
 };
+
+template <class Object, class Function>
+std::function<void(SceneNode&, sf::Time)>
+    derivedAction(Function fn)
+{
+    return [=] (SceneNode& node, sf::Time dt)
+    {
+        // Check if cast is safe
+        assert(dynamic_cast<Object*>(&node) != nullptr);
+        // Downcast node and invoke function on it
+        fn(static_cast<Object&>(node), dt);
+    };
+}
