be408b48dfe318faef897e4dee45e9e05c9ae335
Nguyễn Hoàng Phúc <nhplhp2004@gmail.com>
2023-04-03 07:57:55 +0700

    define Button constructor

--- src/Button.cpp
+++ src/Button.cpp
@@ -2,7 +2,18 @@
 
 Button::Button(sf::Font &font, const std::string &text, sf::Vector2f position, sf::Vector2f size, sf::Color background_color)
 {
-    
+    mBackground.setSize(size);
+    mBackground.setFillColor(background_color);
+    mBackground.setPosition(position);
+
+    mText.setFont(font);
+    mText.setString(text);
+    mText.setCharacterSize(ButtonConfig::CHARACTER_SIZE);
+    mText.setFillColor(ButtonConfig::TEXT_COLOR);
+
+    sf::FloatRect textRect = mText.getLocalBounds();
+    mText.setOrigin(textRect.left + textRect.width / 2.f, textRect.top + textRect.height / 2.f);
+    mText.setPosition(position + size / 2.f);
 }
 
 int Button::getCategory() const
