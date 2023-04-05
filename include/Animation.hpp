78f3b9af74797bc5419cffc25ff2bb909e42622d
Nguyễn Hoàng Phúc <nhplhp2004@gmail.com>
2023-04-05 20:32:11 +0700

    add Animation

---
+++ include/Animation.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <SceneNode.hpp>
+
+struct Animation
+{
+    typedef std::unique_ptr<Animation> Ptr;
+    unsigned int    category;
+    sf::Time        duration;
+    sf::Time        elapsedTime;
+    std::function<void(SceneNode&, sf::Time, sf::Time)> animator;
+};
+
+template <class Object, class Function>
+std::function<void(SceneNode&, sf::Time, sf::Time)>
+    derivedAnimator(Function fn)
+{
+    return [=] (SceneNode& node, sf::Time elapsedTime, sf::Time duration)
+    {
+        // Check if cast is safe
+        assert(dynamic_cast<Object*>(&node) != nullptr);
+        // Downcast node and invoke function on it
+        fn(static_cast<Object&>(node), elapsedTime, duration);
+    };
+}
