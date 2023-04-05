a84db0467fca03a65f657725a2098ff0beb16d1a
Nguyễn Hoàng Phúc <nhplhp2004@gmail.com>
2023-04-05 20:56:54 +0700

    add NodeAnimation::Grow

---
+++ include/NodeAnimation.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <Motion.hpp>
+#include <Node.hpp>
+#include <SFML/Graphics.hpp>
+
+namespace NodeAnimation
+{
+    struct Grow
+    {
+        Grow(){}
+        void operator() (Node& node, sf::Time elapsedTime, sf::Time duration) const
+        {
+            float factor=Motion::Bezier(elapsedTime/duration);
+            node.setScale(factor,factor);
+        }
+    };
+}
