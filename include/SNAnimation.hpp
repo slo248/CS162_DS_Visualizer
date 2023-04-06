33634eff21a14dd6dd23371672f15c4d03e49463
Nguyễn Hoàng Phúc <nhplhp2004@gmail.com>
2023-04-06 19:43:38 +0700

    add SNAnimation

---
+++ include/SNAnimation.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <Motion.hpp>
+#include <SceneNode.hpp>
+
+namespace SNAnimation
+{
+    struct Grow
+    {
+        Grow(){}
+        void operator() (SceneNode& node, sf::Time elapsedTime, sf::Time duration) const
+        {
+            float factor=Motion::Bezier(elapsedTime/duration);
+            node.setScale(factor,factor);
+        }
+    };
+
+    struct Shrink
+    {
+        Shrink(){}
+        void operator() (SceneNode& node, sf::Time elapsedTime, sf::Time duration) const
+        {
+            float factor=1.f-Motion::Bezier(elapsedTime/duration);
+            node.setScale(factor,factor);
+        }
+    };
+}
