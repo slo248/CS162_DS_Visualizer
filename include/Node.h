1d071806be553aba4525c7481bc94cd24c9e89fd
Nguyễn Hoàng Phúc <nhplhp2004@gmail.com>
2023-04-10 09:57:42 +0700

    init Node

---
+++ include/Node.h
@@ -0,0 +1,9 @@
+#include <SFML/Graphics.hpp>
+
+struct Node
+{
+    int value;
+    sf::Vector2f position;
+    Node(int value=0, sf::Vector2f position=sf::Vector2f(0, 0)) : 
+        value(value), position(position) {}
+};
