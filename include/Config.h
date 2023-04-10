2c095f0be2a6ca2a3b33553b23a3ef58bc04f2b4
Nguyễn Hoàng Phúc <nhplhp2004@gmail.com>
2023-04-10 21:31:37 +0700

    add Config.h

---
+++ include/Config.h
@@ -0,0 +1,17 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+namespace CircleConfig
+{
+    const float RADIUS = 20.f;
+    const float THICKNESS = 6.f;
+}
+
+namespace ArrowConfig
+{
+    const float BAR_WIDTH = 50.f;
+    const float TRIANGLE_WIDTH = 10.f;
+    const float HEIGHT_EACH=5.f;
+}
+
+#endif // CONFIG_H
--- include/Figure.h
+++ include/Figure.h
@@ -1,20 +1,8 @@
 #ifndef FIGURE_H
 #define FIGURE_H
 
+#include "Config.h"
 #include "ArrowFigure.h"
-
-namespace CircleConfig
-{
-    const float RADIUS = 20.f;
-    const float THICKNESS = 6.f;
-}
-
-namespace ArrowConfig
-{
-    const float BAR_WIDTH = 50.f;
-    const float TRIANGLE_WIDTH = 10.f;
-    const float HEIGHT_EACH=5.f;
-}
 
 struct Figure
 {
