e3d77e683080f14c50d531ec3d26871d967f9c4b
Nguyễn Hoàng Phúc <nhplhp2004@gmail.com>
2023-04-10 10:12:37 +0700

    update Node::draw(): set position

--- src/Node.cpp
+++ src/Node.cpp
@@ -2,6 +2,7 @@
 
 void Node::draw(sf::RenderWindow *window, sf::CircleShape *circle, sf::Color inColor, sf::Color outColor, float nothing)
 {
+    circle->setPosition(position);
     circle->setFillColor(inColor);
     circle->setOutlineColor(outColor);
     window->draw(*circle);
