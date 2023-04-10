8b6b14ac12686a26c0f688d61f4e14956a63e956
Nguyễn Hoàng Phúc <nhplhp2004@gmail.com>
2023-04-10 13:28:54 +0700

    define ListNode::getPrev()

--- include/ListNode.h
+++ include/ListNode.h
@@ -8,14 +8,30 @@ struct ListNode
     ListNode<T>* next=nullptr;
     ListNode<T>* prev=nullptr;
     ListNode(T data=T()):data(data){}
+
     ListNode<T>* getNext(int cnt=1);
+    ListNode<T>* getPrev(int cnt=1);
 };
 
 template <class T>
 inline ListNode<T> *ListNode<T>::getNext(int cnt)
 {
     ListNode<T>* cur=this;
-    while(cnt--) cur=cur->next;
+    while(cnt--){ 
+        if(cur->next==nullptr) break;
+        cur=cur->next;
+    }
+    return cur;
+}
+
+template <class T>
+inline ListNode<T> *ListNode<T>::getPrev(int cnt)
+{
+    ListNode<T>* cur=this;
+    while(cnt--){ 
+        if(cur->prev==nullptr) break;
+        cur=cur->prev;
+    }
     return cur;
 }
 
