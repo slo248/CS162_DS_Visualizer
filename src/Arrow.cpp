1362a4a11205812063f8b5c8d89bb0971d38eb47
Nguyễn Hoàng Phúc <nhplhp2004@gmail.com>
2023-04-10 21:33:21 +0700

    change Arrow::update()

--- src/Arrow.cpp
+++ src/Arrow.cpp
@@ -1,8 +1,9 @@
 #include "Arrow.h"
+#include "Config.h"
 
 void Arrow::update(ArrowFigure *arrowFig)
 {
-    arrowFig->setPosition(src->position);
+    arrowFig->setPosition(src->position+sf::Vector2f(CircleConfig::RADIUS,0));
 }
 
 void Arrow::draw(sf::RenderWindow *window, ArrowFigure *arrowFig, sf::Color color)
