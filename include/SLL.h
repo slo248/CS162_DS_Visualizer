1378549bd4cbd00b3ac94c75f03469e087299c7d
Nguyễn Hoàng Phúc <nhplhp2004@gmail.com>
2023-04-10 16:23:16 +0700

    add SLL

---
+++ include/SLL.h
@@ -0,0 +1,16 @@
+#ifndef SLL_H
+#define SLL_H
+
+#include "Graph.h"
+
+class SLL_H
+{
+private:
+    sf::RenderWindow& window;
+    
+    Graph graph;
+
+    List<Node> listNode;
+};
+
+#endif // SLL_H
