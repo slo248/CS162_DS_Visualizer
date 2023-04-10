be26a29b67f2f441528ccd9ceb345b43dc5ce705
Nguyễn Hoàng Phúc <nhplhp2004@gmail.com>
2023-04-10 10:03:25 +0700

    add HNeed.h

---
+++ include/HNeed.h
@@ -0,0 +1,17 @@
+#ifndef HNEED_H
+#define HNEED_H
+
+#include <SFML/Graphics.hpp>
+
+#include <iostream>
+#include <fstream>
+#include <functional>
+#include <cassert>
+#include <cmath>
+
+enum CircleType
+{
+    Hollow, Solid
+};
+
+#endif // HNEED_H
