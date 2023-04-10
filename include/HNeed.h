38c0f337ba60b05a9b90596e45327bf3f857387b
Nguyễn Hoàng Phúc <nhplhp2004@gmail.com>
2023-04-10 10:55:58 +0700

    add colors

--- include/HNeed.h
+++ include/HNeed.h
@@ -15,6 +15,17 @@ enum CircleType
     Hollow, Solid
 };
 
-typedef std::function<float(float)> functor;
+typedef std::function<void(float)> functor;
+
+namespace Colors
+{
+    const sf::Color BLANK = sf::Color(0, 0, 0, 0);
+    const sf::Color BLACK = sf::Color(0, 0, 0, 255);
+    const sf::Color WHITE = sf::Color(255, 255, 255, 255);
+    const sf::Color ORANGE = sf::Color(255, 138, 39, 255);
+    const sf::Color GREEN = sf::Color(82, 188, 105, 255);
+    const sf::Color BLUE = sf::Color(46, 187, 209, 255);
+    const sf::Color RED = sf::Color(217, 81, 60, 255);
+}
 
 #endif // HNEED_H
