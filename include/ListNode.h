c946c3a0cfd1200bd887154b8e02aac9d379d6ce
Nguyễn Hoàng Phúc <nhplhp2004@gmail.com>
2023-04-10 13:26:43 +0700

    define ListNode<T>::getNext()

--- include/ListNode.h
+++ include/ListNode.h
@@ -5,11 +5,18 @@ template<class T>
 struct ListNode
 {
     T data;
-    ListNode<T>* next;
-    ListNode<T>* prev;
-    ListNode();
-    ListNode(T data);
-    ~ListNode();
+    ListNode<T>* next=nullptr;
+    ListNode<T>* prev=nullptr;
+    ListNode(T data=T()):data(data){}
+    ListNode<T>* getNext(int cnt=1);
 };
+
+template <class T>
+inline ListNode<T> *ListNode<T>::getNext(int cnt)
+{
+    ListNode<T>* cur=this;
+    while(cnt--) cur=cur->next;
+    return cur;
+}
 
 #endif // LIST_NODE_H
