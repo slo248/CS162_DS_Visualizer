5e40cbf130a41a61004ccac482f21e0af51cf7b1
Nguyễn Hoàng Phúc <nhplhp2004@gmail.com>
2023-04-10 10:16:47 +0700

    init Graph

---
+++ include/Graph.h
@@ -0,0 +1,13 @@
+#include "HNeed.h"
+
+class Graph
+{
+private:
+    sf::RenderWindow* window;
+    sf::CircleShape* hCircle;
+    sf::CircleShape* sCircle;
+
+    int curFrame, curStep;
+    std::vector<int> nFrames;
+    std::vector<std::vector<functor>> drawFunc;
+};
