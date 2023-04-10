0b2448368705f50f7b5d8fa24ca124e73069080e
Nguyễn Hoàng Phúc <nhplhp2004@gmail.com>
2023-04-10 20:43:07 +0700

    define Figure::genArrow()

--- include/Figure.h
+++ include/Figure.h
@@ -9,14 +9,24 @@ namespace CircleConfig
     const float THICKNESS = 6.f;
 }
 
+namespace ArrowConfig
+{
+    const float BAR_WIDTH = 30.f;
+    const float TRIANGLE_WIDTH = 6.f;
+    const float HEIGHT_EACH=5.f;
+}
+
 struct Figure
 {
     sf::CircleShape* hCircle; // hollow circle
     sf::CircleShape* sCircle; // solid circle
+    sf::RectangleShape* bar; // body of an arrow
+    sf::ConvexShape* triangle; // arrow head
     Figure();
     ~Figure();
     void genHollowCircle();
     void genSolidCircle();
+    void genArrow();
 };
 
 #endif // FIGURE_H
--- src/Figure.cpp
+++ src/Figure.cpp
@@ -4,12 +4,15 @@ Figure::Figure()
 {
     genHollowCircle();
     genSolidCircle();
+    genArrow();
 }
 
 Figure::~Figure()
 {
     delete hCircle;
     delete sCircle;
+    delete bar;
+    delete triangle;
 }
 
 void Figure::genHollowCircle()
@@ -37,4 +40,31 @@ void Figure::genSolidCircle()
             bounds.top + bounds.height / 2.f
         );
     }
+}
+
+void Figure::genArrow()
+{
+    bar = new sf::RectangleShape;
+    bar->setSize(sf::Vector2f(ArrowConfig::BAR_WIDTH, ArrowConfig::HEIGHT_EACH));
+    {
+        sf::FloatRect bounds = bar->getLocalBounds();
+        bar->setOrigin(
+            0,
+            bounds.top + bounds.height / 2.f
+        );
+    }
+
+    triangle = new sf::ConvexShape;
+    triangle->setPointCount(3);
+    triangle->setPoint(0, sf::Vector2f(0, 0));
+    triangle->setPoint(1, sf::Vector2f(ArrowConfig::TRIANGLE_WIDTH, (3/2)*ArrowConfig::HEIGHT_EACH));
+    triangle->setPoint(2, sf::Vector2f(0, 3*ArrowConfig::HEIGHT_EACH));
+    {
+        sf::FloatRect bounds = triangle->getLocalBounds();
+        triangle->setOrigin(
+            0,
+            bounds.top + bounds.height / 2.f
+        );
+    }
+    triangle->move(sf::Vector2f(ArrowConfig::BAR_WIDTH, 0));
 }
