f0092ae0d0baa1626d8df5e6b0750cdada99bc98
Nguyễn Hoàng Phúc <nhplhp2004@gmail.com>
2023-04-11 19:26:24 +0700

    define Graph::drawTail()

--- include/Graph.h
+++ include/Graph.h
@@ -23,6 +23,7 @@ public:
     void drawFadeOut(Node* node, sf::Color inColor, sf::Color outColor, sf::Color numColor);
     void drawMove(Node* node, sf::Vector2f src, sf::Vector2f dest,sf::Color inColor, sf::Color outColor, sf::Color numColor);
     void drawHead(Node* node, sf::Color inColor, sf::Color outColor, sf::Color numColor);
+    void drawTail(Node* node, sf::Color inColor, sf::Color outColor, sf::Color numColor);
     //
 
     // List of nodes
--- src/Graph.cpp
+++ src/Graph.cpp
@@ -73,6 +73,11 @@ void Graph::drawHead(Node *node, sf::Color inColor, sf::Color outColor, sf::Colo
     drawFunc.back().push_back(std::bind(&Node::drawHead, node, window, circle, inColor, outColor, text, numColor, std::placeholders::_1));
 }
 
+void Graph::drawTail(Node *node, sf::Color inColor, sf::Color outColor, sf::Color numColor)
+{
+    drawFunc.back().push_back(std::bind(&Node::drawTail, node, window, circle, inColor, outColor, text, numColor, std::placeholders::_1));
+}
+
 void Graph::draw(List<Node> *list, int i, sf::Color inColor, sf::Color outColor, sf::Color numColor)
 {
     draw(&list->begin()->getNext(i)->data, inColor, outColor, numColor);
