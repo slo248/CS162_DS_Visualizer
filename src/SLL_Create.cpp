8a43947e37124b30a111c7607eeea144782a80b3
Nguyễn Hoàng Phúc <nhplhp2004@gmail.com>
2023-04-15 14:38:12 +0700

    SLL: take create codes from SLL.cpp to SLL_Create.cpp

---
+++ src/SLL_Create.cpp
@@ -0,0 +1,53 @@
+#include "SLL.h"
+#include "Random.h"
+#include "Config.h"
+
+void SLL::empty()
+{
+    listNode.clear();
+    listArrow.clear();
+}
+
+void SLL::loadFromFile(std::string path)
+{
+    empty();
+
+    std::ifstream fi(path);
+    if(!fi.is_open()) return;
+    int val;
+    while(fi>>val){
+        assert(0<=val && val<=99);
+        if(listNode.size()==10) break;
+        listNode.pushBack(Node(val));
+    }
+    fi.close();
+}
+
+void SLL::randomList(int n)
+{
+    empty();
+    while(n--)
+        listNode.pushBack(Node(getRand(0,99)));
+}
+
+void SLL::makeList()
+{
+    if(listNode.empty()) return;
+
+    ListElement<Node>* node=listNode.begin();
+    node->data.position=START_POSITION;
+    for(node=node->next; node!=listNode.end(); node=node->next){
+        node->data.position=node->prev->data.position+sf::Vector2f(DISTANCE, 0);
+        listArrow.pushBack(Arrow(&node->prev->data, &node->data));
+    }
+
+    // add new steps
+    graph.addStep(0.5*FPS);
+
+    // draw nodes to graph
+    graph.drawGrow(&listNode,Colors::WHITE,Colors::BLACK,Colors::BLACK);
+    graph.drawSubscript(&listNode.begin()->data,"head",Colors::RED);
+    
+    // draw arrows to graph
+    graph.drawGrow(&listArrow,Colors::BLACK);
+}
