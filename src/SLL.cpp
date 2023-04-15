264cb33e34ddbaa3e2ba6fa0e189c6ad079cdbfc
Nguyễn Hoàng Phúc <nhplhp2004@gmail.com>
2023-04-15 11:25:41 +0700

    SLL::deleteMiddle(): delete del (step 5)

--- src/SLL.cpp
+++ src/SLL.cpp
@@ -747,6 +747,22 @@ void SLL::deleteMiddle(int pos)
         graph.draw(deletedA,Colors::ORANGE);
         graph.draw(&listArrow,pos,listArrow.size()-1,Colors::BLACK);
         //
+
+        // step 5: delete del
+        graph.addStep(0.5*FPS);
+
+        graph.drawSubscript(&listNode.begin()->data,"head",Colors::RED);
+        graph.draw(&listNode,0,pos-2,Colors::WHITE,Colors::ORANGE,Colors::ORANGE);
+        graph.draw(&listNode,pos-1,Colors::ORANGE,Colors::ORANGE,Colors::WHITE);
+        graph.draw(&listNode,pos,n-1,Colors::WHITE,Colors::BLACK,Colors::BLACK);
+        graph.drawShrink(deletedN,Colors::RED,Colors::RED,Colors::WHITE);
+        if(pos<n)
+            graph.draw(&listNode.begin()->getNext(pos)->data,Colors::GREEN,Colors::GREEN,Colors::WHITE);
+
+        graph.draw(&listArrow,0,pos-1,Colors::ORANGE);
+        graph.drawShrink(deletedA,Colors::ORANGE);
+        graph.draw(&listArrow,pos,listArrow.size()-1,Colors::BLACK);
+        //
     }
 }
 
