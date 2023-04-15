40c28d6f07f0abd28f814f4e12c3eb71b9d29bd4
Nguyễn Hoàng Phúc <nhplhp2004@gmail.com>
2023-04-15 14:42:49 +0700

    SLL: take update code from SLL.cpp to SLL_Update.cpp

--- src/SLL.cpp
+++ src/SLL.cpp
@@ -256,34 +256,6 @@ void SLL::processInput()
         graph.setVisualType(AUTO);
 }
 
-void SLL::update()
-{
-    createBtn->update(window);
-    if(curBtn==Button::CREATE){
-        emptyBtn->update(window);
-        randomBtn->update(window);
-        loadFromFileBtn->update(window);
-    }
-
-    insertBtn->update(window);
-    if(curBtn==Button::INSERT){
-        insertFrontBtn->update(window);
-        insertBackBtn->update(window);
-        insertMiddleBtn->update(window);
-    }
-
-    updateBtn->update(window);
-
-    searchBtn->update(window);
-
-    deleteBtn->update(window);
-    if(curBtn==Button::DELETE){
-        deleteFirstBtn->update(window);
-        deleteLastBtn->update(window);
-        deleteMiddleBtn->update(window);
-    }
-}
-
 void SLL::render()
 {
     window->clear(Colors::WHITE);
---
+++ src/SLL_Update.cpp
@@ -0,0 +1,31 @@
+#include "SLL.h"
+#include "Random.h"
+#include "Config.h"
+
+void SLL::update()
+{
+    createBtn->update(window);
+    if(curBtn==Button::CREATE){
+        emptyBtn->update(window);
+        randomBtn->update(window);
+        loadFromFileBtn->update(window);
+    }
+
+    insertBtn->update(window);
+    if(curBtn==Button::INSERT){
+        insertFrontBtn->update(window);
+        insertBackBtn->update(window);
+        insertMiddleBtn->update(window);
+    }
+
+    updateBtn->update(window);
+
+    searchBtn->update(window);
+
+    deleteBtn->update(window);
+    if(curBtn==Button::DELETE){
+        deleteFirstBtn->update(window);
+        deleteLastBtn->update(window);
+        deleteMiddleBtn->update(window);
+    }
+}
