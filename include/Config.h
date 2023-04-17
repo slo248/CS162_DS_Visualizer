026ce5ad883049c9faf17606dcf627029e59c4fc
Nguyễn Hoàng Phúc <nhplhp2004@gmail.com>
2023-04-17 20:03:23 +0700

    add ButtonConfig::CHAR_SIZE

--- include/Config.h
+++ include/Config.h
@@ -5,26 +5,27 @@
 
 namespace CircleConfig
 {
-    const float RADIUS = 20.f;
-    const float THICKNESS = 3.f;
-    const float HALF_WIDTH=RADIUS+THICKNESS;
+    const float RADIUS              = 20.f;
+    const float THICKNESS           = 3.f;
+    const float HALF_WIDTH          = RADIUS+THICKNESS;
 }
 
 namespace ArrowConfig
 {
-    const float BAR_WIDTH = 50.f;
-    const float TRIANGLE_WIDTH = 10.f;
-    const float HEIGHT_EACH=3.f;
-    const float WIDTH=BAR_WIDTH+TRIANGLE_WIDTH;
+    const float BAR_WIDTH           = 50.f;
+    const float TRIANGLE_WIDTH      = 10.f;
+    const float HEIGHT_EACH         = 3.f;
+    const float WIDTH               = BAR_WIDTH+TRIANGLE_WIDTH;
 }
 
 namespace ButtonConfig
 {
-    const float HEIGHT = 60.f;
-    const float WIDTH = 100.f;
-    const sf::Color BG_COLOR = GREEN;
-    const sf::Color BG_MOUSE_OVER = BLACK;
-    const sf::Color TEXT_COLOR = WHITE;
+    const float HEIGHT              = 60.f;
+    const float WIDTH               = 100.f;
+    const float CHAR_SIZE           = 30.f;
+    const sf::Color BG_COLOR        = GREEN;
+    const sf::Color BG_MOUSE_OVER   = BLACK;
+    const sf::Color TEXT_COLOR      = WHITE;
 }
 
 #endif // CONFIG_H
