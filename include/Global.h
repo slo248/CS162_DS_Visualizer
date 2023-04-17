8764b4358f599b5e0b621efc38a742db1106bf37
Nguyễn Hoàng Phúc <nhplhp2004@gmail.com>
2023-04-17 19:53:44 +0700

    add Colors.h

--- include/Global.h
+++ include/Global.h
@@ -1,6 +1,8 @@
 #ifndef HNEED_H
 #define HNEED_H
 
+#include "Colors.h"
+
 #include <SFML/Graphics.hpp>
 
 #include <iostream>
@@ -14,16 +16,5 @@ typedef std::function<void(float)> functor;
 
 enum VisualDir {FORWARD, BACKWARD};
 enum VisualType {AUTO, STEP_BY_STEP};
-
-const sf::Color BLANK = sf::Color(0, 0, 0, 0);
-const sf::Color BLACK = sf::Color(0, 0, 0, 255);
-const sf::Color WHITE = sf::Color(255, 255, 255, 255);
-const sf::Color ORANGE = sf::Color(255, 138, 39, 255);
-const sf::Color GREEN = sf::Color(82, 188, 105, 255);
-const sf::Color BLUE = sf::Color(46, 187, 209, 255);
-const sf::Color RED = sf::Color(217, 81, 60, 255);
-const sf::Color SILVER = sf::Color(192, 192, 192, 255);
-const sf::Color GRAY = sf::Color(128, 128, 128, 255);
-
 
 #endif // HNEED_H
