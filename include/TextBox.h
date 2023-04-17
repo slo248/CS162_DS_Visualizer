3e6f456ad88c24ee077841bd3992995e330bc924
Nguyễn Hoàng Phúc <nhplhp2004@gmail.com>
2023-04-17 07:43:58 +0700

    add TextBoxConfig

--- include/TextBox.h
+++ include/TextBox.h
@@ -3,6 +3,15 @@
 
 #include "HNeed.h"
 
+namespace TextBoxConfig
+{
+    const sf::Vector2f      SIZE(400, 100);
+    const sf::Vector2f      POSITION(0, 0);
+    const sf::Color         BG_COLOR(Colors::BLUE);
+    const sf::Color         TEXT_COLOR(Colors::BLACK);
+    const float             CHAR_SIZE(20);
+}
+
 class TextBox
 {
 public:
