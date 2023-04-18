1a1e2c11f4ed478accb47566a59c8d435b3c1ca9
Nguyễn Hoàng Phúc <nhplhp2004@gmail.com>
2023-04-18 15:40:04 +0700

    define TextBox::handleEvent()

--- include/TextBox.h
+++ include/TextBox.h
@@ -11,12 +11,15 @@ public:
         sf::Vector2f position
     );
     
+    void handleEvent(sf::Event& event, sf::RenderWindow* window);
     void draw(sf::RenderTarget& target, sf::RenderStates states) const;
     
 private:
     sf::RectangleShape  mRect;
     sf::Text            mText;
     std::string         mStr;
+
+    bool                mIsFocus;
 };
 
 #endif // TEXT_BOX_H
--- src/TextBox.cpp
+++ src/TextBox.cpp
@@ -3,7 +3,8 @@
 
 using namespace Config::TextBox;
 
-TextBox::TextBox(sf::Font *font, sf::Vector2f position): mStr("1")
+TextBox::TextBox(sf::Font *font, sf::Vector2f position): 
+    mStr("1"), mIsFocus(false)
 {
     mRect.setSize(sf::Vector2f(WIDTH, HEIGHT));
     mRect.setFillColor(BG_COLOR);
@@ -24,6 +25,22 @@ TextBox::TextBox(sf::Font *font, sf::Vector2f position): mStr("1")
         position.x + 10.f,
         position.y + HEIGHT / 2.0f
     );
+}
+
+void TextBox::handleEvent(sf::Event &event, sf::RenderWindow *window)
+{
+    switch (event.type)
+    {
+        case sf::Event::MouseButtonReleased:
+            if (event.mouseButton.button == sf::Mouse::Left)
+            {
+                sf::Vector2f mousePos = window->mapPixelToCoords(
+                    sf::Vector2i(event.mouseButton.x, event.mouseButton.y)
+                );
+                mIsFocus=mRect.getGlobalBounds().contains(mousePos);
+            }
+            break;
+    }
 }
 
 void TextBox::draw(sf::RenderTarget &target, sf::RenderStates states) const
