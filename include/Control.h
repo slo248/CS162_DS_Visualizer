da271187cc62603607136034ea2428532e6fb7f3
Nguyễn Hoàng Phúc <nhplhp2004@gmail.com>
2023-04-18 07:26:10 +0700

    add control

---
+++ include/Control.h
@@ -0,0 +1,16 @@
+#ifndef CONTROL_H
+#define CONTROL_H
+
+#include "Global.h"
+#include "Button.h"
+
+class Control
+{
+protected:
+    std::vector<Button*> options;
+    std::vector<std::vector<std::string>> suboption;
+    sf::Texture  btnSprite;
+    sf::Texture  bgSprite;
+};
+
+#endif // CONTROL_H
