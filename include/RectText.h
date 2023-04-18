07f0a42a7d0281eb4788cb8ed74896c6d9286cdd
Nguyễn Hoàng Phúc <nhplhp2004@gmail.com>
2023-04-18 07:11:46 +0700

    add RectText

---
+++ include/RectText.h
@@ -0,0 +1,20 @@
+#ifndef RECT_TEXT_H
+#define RECT_TEXT_H
+
+#include "Global.h"
+
+class RectText : public sf::Drawable
+{
+public:
+    RectText(
+        sf::Font*       font,
+        sf::Texture*    bgTexture,
+        std::string     str,
+        sf::Vector2f    position
+    );
+protected:
+    sf::Sprite*    mRect;
+    sf::Text       mText;
+};
+
+#endif // RECT_TEXT_H
