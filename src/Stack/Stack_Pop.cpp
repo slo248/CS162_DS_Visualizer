ba42743460ed2b35bd76bb102b182dea736b28c7
Nguyễn Hoàng Phúc <nhplhp2004@gmail.com>
2023-04-21 21:26:59 +0700

    Stack::popWhenSingle(): add step 3

--- src/Stack/Stack_Pop.cpp
+++ src/Stack/Stack_Pop.cpp
@@ -27,11 +27,17 @@ void Stack::popWhenSingle()
     // step 2: head=head.next
     graph.addStep(0.5*FPS);
 
-    graph.addStep(0.5*FPS);
-
     graph.draw(deletedN,ORANGE,ORANGE,WHITE);
     graph.drawSubscript(deletedN,"tmp",RED);
     graph.draw(&codeBox,2);
+    //
+    
+    // step 3: delete tmp
+    graph.addStep(0.5*FPS);
+
+    graph.drawShrink(deletedN,ORANGE,ORANGE,WHITE);
+    graph.drawSubscript(deletedN,"tmp",RED);
+    graph.draw(&codeBox,3);
     //
 
     deletedNode.push_back(deletedN);
@@ -39,7 +45,13 @@ void Stack::popWhenSingle()
 
 void Stack::pop()
 {
-    if(listNode.size()<=1) popWhenSingle();
+    if(listNode.size()<=1){ 
+        popWhenSingle();
+        return;
+    }
+
+    graph.finishAllSteps();
+    codeBox.loadFromFile("code/Stack/pop.txt");
 
     Node* deletedN=listNode.popFront();
     Arrow* deletedA=listArrow.popFront();
@@ -83,6 +95,7 @@ void Stack::pop()
     graph.drawFadeIn(&listNode.begin()->data,GREEN,GREEN,WHITE);
     if(deletedA) graph.drawShrink(deletedA,GREEN);
     graph.draw(&listArrow,BLACK);
+    graph.drawSubscript(deletedN,"tmp",RED);
     graph.drawSubscript(&listNode.begin()->data,"head",RED);
     graph.draw(&codeBox,3);
     //
