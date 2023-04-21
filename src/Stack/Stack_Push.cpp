7231c57fbc621ed9af1a872f25aa44395c0a294d
Nguyễn Hoàng Phúc <nhplhp2004@gmail.com>
2023-04-21 20:47:27 +0700

    Stack::pushWhenEmpty(): add step 1

--- src/Stack/Stack_Push.cpp
+++ src/Stack/Stack_Push.cpp
@@ -5,4 +5,14 @@ void Stack::pushWhenEmpty(int value)
     assert(listNode.empty());
 
     codeBox.loadFromFile("code/Stack/push.txt");
+
+    listNode.pushBack(value);
+
+    // step 1: appear new node
+    graph.addStep(0.5*FPS);
+
+    graph.drawGrow(&listNode,ORANGE,ORANGE,WHITE);
+    graph.drawSubscript(&listNode.begin()->data,"vtx",RED);
+    graph.draw(&codeBox,0);
+    //
 }
