4aaf5ed1def59751c5fb202ae8d11803e39bf23e
Nguyễn Hoàng Phúc <nhplhp2004@gmail.com>
2023-04-21 21:15:14 +0700

    Stack::pop(): add step 2

--- src/Stack/Stack_Pop.cpp
+++ src/Stack/Stack_Pop.cpp
@@ -26,6 +26,22 @@ void Stack::pop()
     graph.draw(&codeBox,1);
     //
 
+    // step 2: head=head.next
+    graph.addStep(0.5*FPS);
+
+    graph.draw(deletedN,ORANGE,ORANGE,WHITE);
+    graph.draw(&listNode,WHITE,BLACK,BLACK);
+    graph.drawFadeIn(&listNode.begin()->data,GREEN,GREEN,WHITE);
+    if(deletedA){ 
+        graph.draw(deletedA,BLACK);
+        graph.drawGrow(deletedA,GREEN);
+    }
+    graph.draw(&listArrow,BLACK);
+    graph.drawSubscript(deletedN,"tmp",RED);
+    graph.drawSubscript(&listNode.begin()->data,"head",RED);
+    graph.draw(&codeBox,2);
+    //
+
     deletedNode.push_back(deletedN);
     if(deletedA) deletedArrow.push_back(deletedA);
 }
