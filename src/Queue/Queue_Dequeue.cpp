ce699fb5a7bf429e01d05feda5c759cbc4bce78f
Nguyễn Hoàng Phúc <nhplhp2004@gmail.com>
2023-04-22 18:53:00 +0700

    Queue::dequeueWhenSingle(): add step 1

--- src/Queue/Queue_Dequeue.cpp
+++ src/Queue/Queue_Dequeue.cpp
@@ -7,7 +7,22 @@ void Queue::dequeueWhenSingle()
 
     graph.finishAllSteps();
     codeBox.loadFromFile("code/Queue/dequeue.txt");
+    
+    if(listNode.size()==1){
+        graph.addStep(1);
+        graph.draw(&codeBox,0);
+        return;
+    }
 
     Node* deletedN=listNode.popFront();
     deletedNode.push_back(deletedN);
+
+    // step 1: tmp=head
+    graph.addStep(0.5*FPS);
+
+    graph.draw(deletedN,WHITE,BLACK,BLACK);
+    graph.drawFadeIn(deletedN,ORANGE,ORANGE,WHITE);
+    graph.drawSubscript(deletedN,"head/tail/tmp",RED);
+    graph.draw(&codeBox,1);
+    //
 }
