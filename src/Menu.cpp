207170c081d884cab83ca50a66b57db74dee3976
Nguyễn Hoàng Phúc <nhplhp2004@gmail.com>
2023-04-25 20:14:19 +0700

    change position of buttons in menu

--- src/Menu.cpp
+++ src/Menu.cpp
@@ -9,18 +9,18 @@ Menu::Menu(sf::Font *sanf, sf::Vector2f windowSize)
     title.setPosition(sf::Vector2f(windowSize.x/2-title.getGlobalBounds().width/2, 50));
 
     subtitle.setFont(*sanf);
-    subtitle.setString("visualising data structures and algorithms through animation");
+    subtitle.setString("visualising data structures through animation");
     subtitle.setCharacterSize(30);
     subtitle.setFillColor(BLACK);
     subtitle.setPosition(sf::Vector2f(windowSize.x/2-subtitle.getGlobalBounds().width/2, 150));
 
     btnTexture.loadFromFile("media/image/menuBtn.png");
-    DS.push_back(Button(sanf, &btnTexture, "Singly Linked List", sf::Vector2f(windowSize.x/2 - 100, 250)));
-    DS.push_back(Button(sanf, &btnTexture, "Doubly Linked List", sf::Vector2f(windowSize.x/2, 250)));
-    DS.push_back(Button(sanf, &btnTexture, "Circular Linked List", sf::Vector2f(windowSize.x/2 + 100, 250)));
-    DS.push_back(Button(sanf, &btnTexture, "Stack", sf::Vector2f(windowSize.x/2 - 100, 350)));
-    DS.push_back(Button(sanf, &btnTexture, "Queue", sf::Vector2f(windowSize.x/2, 350)));
-    DS.push_back(Button(sanf, &btnTexture, "Array", sf::Vector2f(windowSize.x/2 + 100, 350)));
+    DS.push_back(Button(sanf, &btnTexture, "Singly Linked List", sf::Vector2f(windowSize.x/2 - btnTexture.getSize().x/2 - 300, 250)));
+    DS.push_back(Button(sanf, &btnTexture, "Doubly Linked List", sf::Vector2f(windowSize.x/2 - btnTexture.getSize().x/2, 250)));
+    DS.push_back(Button(sanf, &btnTexture, "Circular Linked List", sf::Vector2f(windowSize.x/2 - btnTexture.getSize().x/2 + 300, 250)));
+    DS.push_back(Button(sanf, &btnTexture, "Stack", sf::Vector2f(windowSize.x/2 - btnTexture.getSize().x/2 - 300, 450)));
+    DS.push_back(Button(sanf, &btnTexture, "Queue", sf::Vector2f(windowSize.x/2 - btnTexture.getSize().x/2, 450)));
+    DS.push_back(Button(sanf, &btnTexture, "Array", sf::Vector2f(windowSize.x/2 - btnTexture.getSize().x/2 + 300, 450)));
 }
 
 void Menu::draw(sf::RenderTarget &target, sf::RenderStates states) const
