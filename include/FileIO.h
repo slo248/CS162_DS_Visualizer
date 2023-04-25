a9b48fb52e95abe9d26405e85be2d586f0ed4c87
Nguyễn Hoàng Phúc <nhplhp2004@gmail.com>
2023-04-25 13:15:19 +0700

    define openWindowExplorer()

---
+++ include/FileIO.h
@@ -0,0 +1,13 @@
+#ifndef FILE_IO_H
+#define FILE_IO_H
+
+#include <windows.h>
+#include <commdlg.h>
+#include <shlobj.h>
+#include <fstream>
+#include <string>
+#include <vector>
+
+std::vector<int> openWindowExplorer();
+
+#endif // FILE_IO_H
