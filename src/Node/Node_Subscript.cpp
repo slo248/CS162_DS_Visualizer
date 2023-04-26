64b18b508f0259c0099287eb01cb7d7c99d0ffc0
Nguyễn Hoàng Phúc <nhplhp2004@gmail.com>
2023-04-26 16:28:20 +0700

    update Node::drawSubscript()

--- src/Node/Node_Subscript.cpp
+++ src/Node/Node_Subscript.cpp
@@ -13,29 +13,31 @@ void Node::drawSubscript(sf::RenderWindow *window, sf::Text *text, std::string s
     }
     text->setFillColor(textColor);
 
+    sf::FloatRect textRect=text->getLocalBounds();
+
     switch (dir)
     {
         case UP:
             text->setPosition(
                 position.x, 
-                position.y-Config::Circle::RADIUS*3.f/2.f
+                position.y-(Config::Circle::HALF_WIDTH+textRect.height/2.f+5.f)
             );
             break;
         case DOWN:
             text->setPosition(
                 position.x, 
-                position.y+Config::Circle::RADIUS*3.f/2.f
+                position.y+(Config::Circle::HALF_WIDTH+textRect.height/2.f+5.f)
             );
             break;
         case LEFT:
             text->setPosition(
-                position.x-Config::Circle::RADIUS*3.f/2.f, 
+                position.x-(Config::Circle::HALF_WIDTH+textRect.width/2.f+5.f), 
                 position.y
             );
             break;
         case RIGHT:
             text->setPosition(
-                position.x+Config::Circle::RADIUS*3.f/2.f, 
+                position.x+(Config::Circle::HALF_WIDTH+textRect.width/2.f+5.f), 
                 position.y
             );
             break;
