9ecaf6df7b57ed316871c36e1b0e57d9816ff983
Nguyễn Hoàng Phúc <nhplhp2004@gmail.com>
2023-04-28 10:26:01 +0700

    DArr::checkSize(): add first step

--- src/DArr/DArr_Insert.cpp
+++ src/DArr/DArr_Insert.cpp
@@ -6,9 +6,8 @@ using namespace Config::DArr;
 void DArr::checkSize()
 {
     if(num.value<curMaxNode) return;
-    
+
     // actually allocate new array with size double of current size
-    saveOldMem();
 
     for(int i=curMaxNode; i<2*curMaxNode; i++){
         listNodeIndex.pushBack(i);
@@ -19,9 +18,31 @@ void DArr::checkSize()
     }
 
     curMaxNode<<=1;
+
+    tmpListNode.clear();
+    for(int i=0; i<curMaxNode; i++){
+        tmpListNode.pushBack(-1);
+        tmpListNode.rbegin()->data.position=START_POSITION_A+sf::Vector2f(i*DISTANCE, 100);
+    }
     //
 
     // update graph
-    
+    // step 1: create new Arr
+    graph.addStep(0.5*FPS);
+
+    graph.draw(&listNodeIndex,SQUARE,0,(curMaxNode>>1)-1,WHITE,BLACK,BLACK);
+    graph.drawGrow(&listNodeIndex,SQUARE,curMaxNode>>1,curMaxNode-1,WHITE,BLACK,BLACK);
+
+    graph.draw(&listNodeA,SQUARE,0,(curMaxNode>>1)-1,WHITE,BLACK,BLACK);
+
+    graph.drawGrow(&tmpListNode,SQUARE,0,curMaxNode-1,WHITE,BLACK,BLACK);
+
+    num.position+=sf::Vector2f(0,100);
+    graph.draw(&num,SQUARE,WHITE,BLACK,BLACK);
+
+    graph.drawSubscript(&listNodeIndex.begin()->data,"index",RED,LEFT);
+    graph.drawSubscript(&listNodeA.begin()->data,"a",RED,LEFT);
+    graph.drawSubscript(&tmpListNode.begin()->data,"newA",RED,LEFT);
+    //
     //
 }
