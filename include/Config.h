4163383980d89a3fb6c8ad00d4d24ad1898cf43d
Nguyễn Hoàng Phúc <nhplhp2004@gmail.com>
2023-05-08 11:12:48 +0700

    add Config::CLL

--- include/Config.h
+++ include/Config.h
@@ -98,6 +98,17 @@ namespace Config
         const int MAX_NODE=10;
     }
 
+    namespace CLL
+    {
+        const int MIN_NODE=0;
+        const int MAX_NODE=10;
+        const int RADIUS=250;
+        const sf::Vector2f CENTER= sf::Vector2f(
+            Window::WIDTH/2.f,
+            Window::HEIGHT/2.f
+        );
+    }
+
     namespace Stack
     {
         const int MIN_NODE=0;
