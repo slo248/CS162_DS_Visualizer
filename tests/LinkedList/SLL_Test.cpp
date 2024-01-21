9f9a18e6886be2b8bef099fd0ac63db3fc3a8de3
Nguyễn Hoàng Phúc <nhplhp2004@gmail.com>
2024-01-21 13:17:58 +0700

    use gtest

--- tests/LinkedList/SLL_Test.cpp
+++ tests/LinkedList/SLL_Test.cpp
@@ -1,16 +1,10 @@
 #include "SLL.h"
 
-#include <iostream>
+#include <gtest/gtest.h>
 
-void createAndDestroy() {
-    std::cerr << "SLL Test: Create and destroy" << std::endl;
+TEST(SLL, CreateAndDestroy) {
     Core::SLL<int> sll;
     Core::SLL<float> sll2;
     Core::SLL<char> sll3;
     Core::SLL<std::string> sll4;
-}
-
-int main() {
-    createAndDestroy();
-    return 0;
 }
